3f219e0066a471dc46770b9a249bf0a3a0012781
Nimesh Akalanka Hettiarachchi <90953455+nimeshakalanka@users.noreply.github.com>
2025-04-17 16:20:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,4 +55,4 @@ def gallery():
     return render_template('gallery.html', images=images)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=80)
